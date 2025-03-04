d128f15c8b18f6d4e5b1153e2a52f4c5cf08b3a6
MarziaCapezzuto <marzia-capezzuto@outlook.it>
2025-03-04 12:37:28 +0100

    Add files via upload

---
+++ Assets/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".btn-progetti").addEventListener("click", function () {
+        window.location.href = "progetti.html";
+    });
+});
